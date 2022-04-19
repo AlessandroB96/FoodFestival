eea89b6a34cbce0de36d2f6e9a3dec4c5a2cddd8
AlessandroB96 <abelaj7196@gmail.com>
2022-04-19 14:50:34 -0400

    added lazy loading for js and img files

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,39 @@ const webpack = require("webpack");
 
 module.exports = { 
 
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js', //bundle file of script.js is app.bundle.js
+        events: './assets/js/events.js', //bundle file of events.js is events.bundle.js
+        schedule: './assets/js/schedule.js', //bundle file of schedule.js is schedule.bundle.js
+        tickets: './assets/js/tickets.js' //bundle file of tickets.js is tickets.bundle.js
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js',
+        filename: '[name].bundle.js',  //the name of each attribute in the entry object will be used in place of [name]
+    },
+    module: {     //module property is where loaders are configured 
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../","/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ] 
     },
     plugins: [
         new webpack.ProvidePlugin({
